4dbc0db1c61fdf77ffe72384c5dacf2481b2cd6a
karpagam12344 <karpagalakshmipadmarajan34@gmail.com>
2025-12-24 19:26:18 +0530

    added new file

--- python.py
+++ python.py
@@ -5,4 +5,4 @@ a2 = a**2 - b**2
 print(abs(a1 - a2))
 # This code calculates the absolute difference between the sum and difference of the squares of two numbers a and b.
 # It first computes a1 as the sum of squares (a^2 + b^2) and a2 as the difference of squares (a^2 - b^2).
-# Finally, it prints the absolute value of the difference between a1 and a2.    
+# Finally, it prints the absolute value of the difference between a1 and a2.
---
+++ python1.py
@@ -0,0 +1,4 @@
+from numpy import random
+x =random.randint(100,size=(5))
+print(x)
+# This code generates an array of 5 random integers between 0 and 99 using NumPy's random module and prints the array.
